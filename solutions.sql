4d609101ba90a6aa9fc8efa34697f5709936cc2e
Manuel Daza <madatascienceml@gmail.com>
2026-06-30 18:51:55 +0200

    LAB sql select solved

---
+++ solutions.sql
@@ -0,0 +1,76 @@
+-- Challenge 1
+
+SELECT 
+	authors.au_id AS "AUTHOR ID",
+	authors.au_lname AS "LAST NAME",
+	authors.au_fname AS "FIRST NAME",
+	titles.title AS "TITLE",
+	publishers.pub_name AS "PUBLISHER"
+FROM authors 
+JOIN titleauthor 
+	ON authors.au_id = titleauthor.au_id
+JOIN titles 
+	ON titles.title_id = titleauthor.title_id
+JOIN publishers 
+	ON publishers.pub_id = titles.pub_id;
+
+
+-- Challenge 2
+
+SELECT 
+	authors.au_id AS "AUTHOR ID",
+	authors.au_lname AS "LAST NAME",
+	authors.au_fname AS "FIRST NAME",
+	publishers.pub_name AS "PUBLISHER",
+	COUNT(titleauthor.title_id) AS "TITLE COUNT"	
+FROM authors 
+JOIN titleauthor 
+	ON authors.au_id = titleauthor.au_id
+JOIN titles 
+	ON titles.title_id = titleauthor.title_id
+JOIN publishers 
+	ON publishers.pub_id = titles.pub_id
+GROUP BY
+    authors.au_id,
+    authors.au_lname,
+    authors.au_fname,
+    publishers.pub_name;
+
+
+-- Challenge 3
+
+SELECT 
+	authors.au_id AS "AUTHOR ID",
+	authors.au_lname AS "LAST NAME",
+	authors.au_fname AS "FIRST NAME",
+	SUM(titles.ytd_sales) AS "TOTAL"	
+FROM authors 
+JOIN titleauthor 
+	ON authors.au_id = titleauthor.au_id
+JOIN titles 
+	ON titles.title_id = titleauthor.title_id
+GROUP BY
+    authors.au_id,
+    authors.au_lname,
+    authors.au_fname
+ORDER BY "TOTAL" DESC
+LIMIT 3;
+
+
+-- Challenge 4
+
+SELECT 
+	authors.au_id AS "AUTHOR ID",
+	authors.au_lname AS "LAST NAME",
+	authors.au_fname AS "FIRST NAME",
+	COALESCE(SUM(titles.ytd_sales), 0) AS "TOTAL"	
+FROM authors 
+LEFT JOIN titleauthor 
+	ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles 
+	ON titles.title_id = titleauthor.title_id
+GROUP BY
+    authors.au_id,
+    authors.au_lname,
+    authors.au_fname
+ORDER BY "TOTAL" DESC;
